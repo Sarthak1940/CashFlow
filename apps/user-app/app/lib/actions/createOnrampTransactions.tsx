101bf75710913f9a42acc8b20d75f5220cb5e97b
Sarthak Godse <sarthak123godse@gmail.com>
2024-07-21 21:48:24 +0530

    add wehbook handler

--- apps/user-app/app/lib/actions/createOnrampTransactions.tsx
+++ apps/user-app/app/lib/actions/createOnrampTransactions.tsx
@@ -2,26 +2,46 @@
 import { getServerSession } from "next-auth"
 import { AUTH_CONFIG } from "../auth"
 import prisma from "@repo/db/client";
+import axios from "axios";
 
 export const createOnrampTransactions = async (provider: string, amount: number) => {
   const session = await getServerSession(AUTH_CONFIG);
-  if (!session || !session.user?.id) {
+  const userId = Number(session?.user?.id)
+
+  if (!userId) {
     return {
       message: "User not authenticated"
     }
   }
+  const token = Date.now().toString()
 
     await prisma.onRampTransaction.create({
       data: {
         status: "Processing",
         provider: provider || "",
         amount: amount,
-        userId: Number(session?.user?.id),
-        token: Date.now().toString(),
+        userId: userId,
+        token: token,
         startTime: new Date()
     }
   })
+
+  webhookHandler(token, userId, amount)
+
   return {
     message: "Done"
+  }
+}
+
+async function webhookHandler(token: string, userId: number, amount: number) {
+  try {
+    await axios.post(" http://localhost:3003/hdfcWebhook", {
+      token: token,
+      user_identifier: userId,
+      amount: amount.toString()
+    })
+  } catch (error) {
+    console.log("Error while getting money", error);
+    
   }
 }
