4359956726a613fff740434cdfc5c0b166250c1b
Sarthak Godse <sarthak123godse@gmail.com>
2024-07-21 21:08:30 +0530

    add basic layout

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Providers } from "../provider";
+import { AppBarClient } from "../components/AppBarClient";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Providers>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <div className="min-w-screen h-[100vh] bg-[#ebe6e6] overflow-hidden">
+            <AppBarClient/>
+           {children}
+          </div>
+        </body>
       </Providers>
     </html>
   );
