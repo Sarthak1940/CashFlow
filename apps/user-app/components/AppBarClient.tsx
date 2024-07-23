b9cdb9803eb17e76bf09ec005004c2565285e672
Sarthak Godse <sarthak123godse@gmail.com>
2024-07-23 18:28:15 +0530

    fix hydration error

--- apps/user-app/components/AppBarClient.tsx
+++ apps/user-app/components/AppBarClient.tsx
@@ -80,7 +80,7 @@ function Menu() {
     <button data-collapse-toggle="navbar-hamburger" type="button" className="lg:hidden inline-flex items-center justify-center p-2 w-10 h-10 text-sm text-white rounded-lg" 
     onClick={() => setMenuOpen(!menuOpen)}>
       <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
       </svg>
     </button>
     <div className={`w-full ${menuOpen ? "block" : "hidden"}`}>
